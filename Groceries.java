41f5e6b0aa85562449e6867d0dc93fc6e75fe730
Siris St Victor <144554416+MadebySiris@users.noreply.github.com>
2024-12-02 17:01:03 -0500

    Create Groceries.java

---
+++ Groceries.java
@@ -0,0 +1,7 @@
+public class Groceries extends Product {
+    //groceries constructor uses super to call product constructor 
+    public Groceries(int productId, int price, int quantity, String name){
+    super(productId, price, quantity,name);
+        
+    }
+}
